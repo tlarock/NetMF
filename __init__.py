4e3a161686ce286a13d7d227e51c962a82657a92
Tim LaRock <timothylarock@gmail.com>
2021-10-28 10:44:27 -0400

    Added init and setup.

---
+++ __init__.py
@@ -0,0 +1,11 @@
+"""
+netmf
+-----
+
+install netmf editable to avoid relative imports
+
+"""
+from .netmf import *
+from .predict import *
+
+__all__ = ["netmf"]
---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name='netmf',
+    version='0.0.1',
+    author='',
+    author_email='',
+    description='',
+    url='',
+    packages=setuptools.find_packages(),
+    classifiers=['Programming Language :: Python :: 3',
+                 'License :: OSI Approved :: MIT License',
+                 'Operating System :: OS Independent']
+)
